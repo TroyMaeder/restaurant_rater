5cfd434cb3c752127327b93107be016305f35338
Troy Maeder <troy.maeder@gmail.com>
2016-09-02 20:34:02 +0100

    Add frontpage route and mongo database

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 
-app.get('/', (req, res) => {
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.get('/frontpage', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
