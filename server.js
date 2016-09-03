373a6395af047fadb5c9d7e51ff13ec641c4af1d
Troy Maeder <troy.maeder@gmail.com>
2016-09-03 14:43:46 +0100

    Add image from api

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const request = require('request');
 
 const app = express();
-const path = require('path');
 
 app.use(express.static(`${__dirname}/views`));
 
@@ -11,24 +10,19 @@ app.set('views', `${__dirname}/views`);
 app.set('view engine', 'jade');
 
 app.get('/', (req, res) => {
-  request('https://developers.zomato.com/api/v2.1/restaurant?apikey=39075b374a5c0d9ee98fcc8e52d0a07c', (error, response, body) => {
+  res.render('login');
+});
+
+app.get('/api', (req, res) => {
+  request('https://developers.zomato.com/api/v2.1/search?apikey=39075b374a5c0d9ee98fcc8e52d0a07c&q=gauchos', (error, response, body) => {
     if (!error && response.statusCode === 200) {
       const json = JSON.parse(body);
-      const apiString = JSON.stringify(json.R);
-      res.render('login');
+      const restaurantImage = json.restaurants[0].restaurant.featured_image;
+      const restaurantJPG = `"${restaurantImage}"`;
+      res.render('restaurants',
+      { title: restaurantJPG }
+    );
     }
   });
 });
-
-app.get('/index', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
-});
-
-// app.get('/api', () => {
-//   request('https://developers.zomato.com/api/v2.1/restaurant?apikey=39075b374a5c0d9ee98fcc8e52d0a07c', (error, response, body) => {
-//     if (!error && response.statusCode === 200) {
-//       const json = JSON.parse(body);
-//     }
-//   });
-// });
 app.listen(8080);
