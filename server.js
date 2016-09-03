01c8ce2c249d7d354985ee83a56c16a55279109a
Troy Maeder <troy.maeder@gmail.com>
2016-09-03 16:40:14 +0100

    Remove quotes around Gauchos image

--- server.js
+++ server.js
@@ -18,11 +18,26 @@ app.get('/api', (req, res) => {
     if (!error && response.statusCode === 200) {
       const json = JSON.parse(body);
       const restaurantImage = json.restaurants[0].restaurant.featured_image;
-      const restaurantJPG = `"${restaurantImage}"`;
+      const restaurantJPG = `${restaurantImage}`;
       res.render('restaurants',
       { title: restaurantJPG }
-    );
+      );
     }
   });
 });
+
+// app.get('/api', (req, res) => {
+//   request(apiCall, (error, response, body) => {
+//     if (!error && response.statusCode === 200) {
+//       const json = JSON.parse(body);
+//       const restaurantImage = json.restaurants[0].restaurant.featured_image;
+//       const restaurantJPG = `'${restaurantImage}'`;
+//       console.log(restaurantJPG);
+//       res.render('restaurants',
+//       { title: restaurantImage}
+//       );
+//     }
+//   });
+// });
+// whatever('https://developers.zomato.com/api/v2.1/search?apikey=39075b374a5c0d9ee98fcc8e52d0a07c&q=gauchos');
 app.listen(8080);
