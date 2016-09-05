17ea11d0061569b116c470a54a3f5b0f25e79e39
Troy Maeder <troy.maeder@gmail.com>
2016-09-05 20:52:29 +0100

    Add restaurants object

--- mongo.js
+++ mongo.js
@@ -1,3 +1,23 @@
-const mongoose = require('mongoose');
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
 
-mongoose.connect('mongodb://localhost/restaurants');
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://localhost:27017/myproject';  
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
--- server.js
+++ server.js
@@ -10,30 +10,45 @@ app.set('views', `${__dirname}/views`);
 app.set('view engine', 'jade');
 
 app.get('/', (req, res) => {
-  res.render('login');
+  res.render('index');
 });
 
 app.get('/api', (req, res) => {
-  request('https://developers.zomato.com/api/v2.1/search?apikey=39075b374a5c0d9ee98fcc8e52d0a07c&q=gauchos', (error, response, body) => {
-    request('https://developers.zomato.com/api/v2.1/search?apikey=39075b374a5c0d9ee98fcc8e52d0a07c&q=tgi', (err, respon, body1) => {
-      if (!error && response.statusCode === 200) {
-        const json = JSON.parse(body);
-        const restaurantImage = json.restaurants[0].restaurant.featured_image;
-        const restaurantName = json.restaurants[0].restaurant.name;
-        const restaurantJPG = `${restaurantImage}`;
-
-        const json1 = JSON.parse(body1);
-        const restaurantImage1 = json1.restaurants[0].restaurant.featured_image;
-        const restaurantJPG1 = `${restaurantImage1}`;
-        console.log(restaurantJPG1)
-        res.render('restaurants',
-        { title1: restaurantJPG,
-          title2: restaurantJPG1
-        }
-        );
+  res.render('restaurants', {
+    //hardcoded
+    restaurants: [
+      {
+        name: 'Gauchos',
+        image: 'https://b.zmtcdn.com/data/pictures/chains/0/6100580/5f113fc379bf1daf5e57f60ca65e7d35_featured_v2.jpg'
+      },
+      {
+        name: "McDonald's",
+        image: '',
       }
-    });
+    ]
+    title1: 'https://b.zmtcdn.com/data/pictures/chains/0/6100580/5f113fc379bf1daf5e57f60ca65e7d35_featured_v2.jpg',
+    title2: 'https://b.zmtcdn.com/data/pictures/chains/0/6100580/5f113fc379bf1daf5e57f60ca65e7d35_featured_v2.jpg'
   });
+  // request('https://developers.zomato.com/api/v2.1/search?apikey=39075b374a5c0d9ee98fcc8e52d0a07c&q=gauchos', (error, response, body) => {
+  //   request('https://developers.zomato.com/api/v2.1/search?apikey=39075b374a5c0d9ee98fcc8e52d0a07c&q=tgi', (err, respon, body1) => {
+  //     if (!error && response.statusCode === 200) {
+  //       const json = JSON.parse(body);
+  //       const restaurantImage = json.restaurants[0].restaurant.featured_image;
+  //       const restaurantName = json.restaurants[0].restaurant.name;
+  //       const restaurantJPG = `${restaurantImage}`;
+  //
+  //       const json1 = JSON.parse(body1);
+  //       const restaurantImage1 = json1.restaurants[0].restaurant.featured_image;
+  //       const restaurantJPG1 = `${restaurantImage1}`;
+  //       console.log(restaurantJPG1)
+  //       res.render('restaurants',
+  //       { title1: restaurantJPG,
+  //         title2: restaurantJPG1
+  //       }
+  //       );
+  //     }
+  //   });
+  // });
 });
 
 
