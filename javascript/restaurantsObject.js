c64927e6730ab42f9895e73b16ec7bf3b80ea891
Troy Maeder <troy.maeder@gmail.com>
2016-09-06 22:41:58 +0100

    ??

---
+++ javascript/restaurantsObject.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    name: 'Gauchos',
+    image: 'https://b.zmtcdn.com/data/pictures/chains/0/6100580/5f113fc379bf1daf5e57f60ca65e7d35_featured_v2.jpg',
+  },
+  {
+    name: "TGI Friday's",
+    image: 'https://www.zomato.com/london/tgi-fridays-stratford/photos#tabtop?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1',
+  },
+];
--- server.js
+++ server.js
@@ -10,47 +10,24 @@ app.set('views', `${__dirname}/views`);
 app.set('view engine', 'jade');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('login ');
 });
 
 app.get('/api', (req, res) => {
-  res.render('restaurants', {
-    //hardcoded
-    restaurants: [
-      {
-        name: 'Gauchos',
-        image: 'https://b.zmtcdn.com/data/pictures/chains/0/6100580/5f113fc379bf1daf5e57f60ca65e7d35_featured_v2.jpg'
-      },
-      {
-        name: "McDonald's",
-        image: '',
+  request('https://developers.zomato.com/api/v2.1/search?apikey=39075b374a5c0d9ee98fcc8e52d0a07c&q=gauchos', (error, response, body) => {
+    request('https://developers.zomato.com/api/v2.1/search?apikey=39075b374a5c0d9ee98fcc8e52d0a07c&q=tgi', (err, respon, body1) => {
+      if (!error && response.statusCode === 200) {
+        const json = JSON.parse(body);
+        const name = 'Gaucho';
+        const restaurantJPG = 'https://b.zmtcdn.com/data/pictures/chains/0/6100580/5f113fc379bf1daf5e57f60ca65e7d35_featured_v2.jpg';
+        const restaurantJPG1 = 'https://b.zmtcdn.com/data/pictures/chains/4/6100234/2c141df6ab0b59ed90a08084264a423b_featured_v2.jpg';
+        res.render('restaurants',
+        { title1: restaurantJPG,
+          title2: restaurantJPG1,
+          title_name: name
+        });
       }
-    ]
-    title1: 'https://b.zmtcdn.com/data/pictures/chains/0/6100580/5f113fc379bf1daf5e57f60ca65e7d35_featured_v2.jpg',
-    title2: 'https://b.zmtcdn.com/data/pictures/chains/0/6100580/5f113fc379bf1daf5e57f60ca65e7d35_featured_v2.jpg'
+    });
   });
-  // request('https://developers.zomato.com/api/v2.1/search?apikey=39075b374a5c0d9ee98fcc8e52d0a07c&q=gauchos', (error, response, body) => {
-  //   request('https://developers.zomato.com/api/v2.1/search?apikey=39075b374a5c0d9ee98fcc8e52d0a07c&q=tgi', (err, respon, body1) => {
-  //     if (!error && response.statusCode === 200) {
-  //       const json = JSON.parse(body);
-  //       const restaurantImage = json.restaurants[0].restaurant.featured_image;
-  //       const restaurantName = json.restaurants[0].restaurant.name;
-  //       const restaurantJPG = `${restaurantImage}`;
-  //
-  //       const json1 = JSON.parse(body1);
-  //       const restaurantImage1 = json1.restaurants[0].restaurant.featured_image;
-  //       const restaurantJPG1 = `${restaurantImage1}`;
-  //       console.log(restaurantJPG1)
-  //       res.render('restaurants',
-  //       { title1: restaurantJPG,
-  //         title2: restaurantJPG1
-  //       }
-  //       );
-  //     }
-  //   });
-  // });
 });
-
-
-
 app.listen(8080);
