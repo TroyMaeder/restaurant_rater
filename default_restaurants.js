7e87a275e10d50abd821fe9b3fa0893c96516075
Troy Maeder <troy.maeder@gmail.com>
2016-10-26 14:12:18 +0100

    Add default restaurants page

---
+++ default_restaurants.js
@@ -0,0 +1,20 @@
+const restaurants = [
+  {
+    name: 'TGI Fridays',
+    picture: 'https://b.zmtcdn.com/data/pictures/chains/4/6100234/2c141df6ab0b59ed90a08084264a423b_featured_v2.jpg',
+    address: '25-29 Coventry St, London W1D 7AG',
+    neighbourhood: 'Covent Garden',
+  },
+  {
+    name: 'Gauchos',
+    picture: 'https://b.zmtcdn.com/data/pictures/chains/0/6100580/5f113fc379bf1daf5e57f60ca65e7d35_featured_v2.jpg',
+    address: '60A Charlotte St',
+    neighbourhood: 'Piccadilly Square',
+  },
+  {
+    name: 'Sushi Samba',
+    picture: 'https://b.zmtcdn.com/data/pictures/6/6107336/90de72609f27b3ef482c0afe6dff50b8_featured_v2.jpg',
+    address: 'Heron Tower, 110 Bishopsgate',
+    neighbourhood: 'City of London',
+  },
+];
