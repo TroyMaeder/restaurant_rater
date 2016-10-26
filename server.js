2a4bb107fc7401245d1c54a5cc86a8e6f387aecc
Troy Maeder <troy.maeder@gmail.com>
2016-10-26 14:10:35 +0100

    Import restaurants array

--- server.js
+++ server.js
@@ -2,51 +2,12 @@ const express = require('express');
 
 const app = express();
 
-const fs = require('fs');
+const restaurants = require('./default_restaurants');
 
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'jade');
 app.use(express.static(`${__dirname}/public/css`));
 app.use(express.static(`${__dirname}/public/js`));
-
-function retrieveWords(restaurantList, userInput) {
-  var suggestedRestaurants = [];
-  for (var i = 0; i < restaurantList.length; i++) {
-    if (restaurantList[i].search(userInput) === 0 && userInput.length > 2) {
-      suggestedRestaurants.push(restaurantList[i]);
-    }
-  }
-  return suggestedRestaurants;
-}
-
-fs.readFile(`${__dirname}/london_restaurants.txt`, 'utf8', (err, data) => {
-  if (err) {
-    throw err;
-  }
-  londonRestaurants = data.split('\n');
-});
-
-
-const restaurants = [
-  {
-    name: 'TGI Fridays',
-    picture: 'https://b.zmtcdn.com/data/pictures/chains/4/6100234/2c141df6ab0b59ed90a08084264a423b_featured_v2.jpg',
-    address: '25-29 Coventry St, London W1D 7AG',
-    neighbourhood: 'Covent Garden',
-  },
-  {
-    name: 'Gauchos',
-    picture: 'https://b.zmtcdn.com/data/pictures/chains/0/6100580/5f113fc379bf1daf5e57f60ca65e7d35_featured_v2.jpg',
-    address: '60A Charlotte St',
-    neighbourhood: 'Piccadilly Square',
-  },
-  {
-    name: 'Sushi Samba',
-    picture: 'https://b.zmtcdn.com/data/pictures/6/6107336/90de72609f27b3ef482c0afe6dff50b8_featured_v2.jpg',
-    address: 'Heron Tower, 110 Bishopsgate',
-    neighbourhood: 'City of London',
-  },
-];
 
 app.get('/', (req, res) => {
   if (res.statusCode === 200) {
