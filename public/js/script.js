127bac5240e6985123d7c93351b94242a263196a
Troy Maeder <troy.maeder@gmail.com>
2016-10-28 23:06:37 +0100

    Add event listener to add restaurant

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,23 @@
+document.getElementsByClassName('predicted_restaurant_wrap')[0].addEventListener('click', function() {
+    console.log('hello');
+});
+
+
 document.getElementById('user_input').addEventListener('keyup', function() {
   const xhr = new XMLHttpRequest();
   const input = document.getElementById('user_input').value;
   xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+    if (this.readyState === 4 && this.status === 200) {
       const arr = JSON.parse(xhr.response);
-      console.log(arr);
       for (let i = 0; i < arr.length; i++) {
-        document.getElementById(`restaurant${i}`).innerHTML = arr[i]
+        document.getElementById(`restaurant${i}`).innerHTML = arr[i];
       }
     }
   };
   if (input) {
     for (let i = 0; i < 3; i++) {
       document.getElementById(`restaurant${i}`).value = '';
-      xhr.open('GET', '/search/' + input, true);
+      xhr.open('GET', `/search/${input}`, true);
       // xhr.open('GET', '/search-service?q=' + input, true);
       xhr.send();
     }
