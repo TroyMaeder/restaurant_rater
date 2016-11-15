d488b06207bd723dc9ea523283d877c9a7b72f07
Troy Maeder <troy.maeder@gmail.com>
2016-11-15 11:28:25 +0000

    Add correct schema for restaurants

--- models/restaurants.js
+++ models/restaurants.js
@@ -1,17 +1,20 @@
-// const db = require('../db');
-// const mongoose = require('mongoose');
-//
-// const Restaurant = db.model('Restaurant', new mongoose.Schema({
-//   restaurant: {
-//     name: String,
-//   },
-// }));
-//
-// exports.findRestaurant = function(userInput, callback) {
-//   const userInputRegEx = new RegExp(userInput, 'i');
-//
-//   Restaurant.find({ 'restaurant.name': userInputRegEx }, { 'restaurant.name': 1, 'restaurant._id': 1 },
-//   (err, restaurants) => {
-//     callback(err, restaurants);
-//   });
-// };
+const mongoose = require('mongoose');
+
+const restaurantSchema = new mongoose.Schema({
+  restaurant: {
+    name: String,
+  },
+});
+
+restaurantSchema.statics.findRestaurant = function(userInput, callback) {
+  const userInputRegEx = new RegExp(userInput, 'i');
+
+  Restaurant.find({ 'restaurant.name': userInputRegEx }, { 'restaurant.name': 1, 'restaurant._id': 1 },
+  (err, restaurants) => {
+    callback(err, restaurants);
+  });
+};
+
+const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+
+module.exports = Restaurant;
