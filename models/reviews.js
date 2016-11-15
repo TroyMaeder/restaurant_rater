834ed82f9deba1d16a321130f3f2933733c2d6ff
Troy Maeder <troy.maeder@gmail.com>
2016-11-15 11:32:05 +0000

    Add correct schema for reviews

--- models/reviews.js
+++ models/reviews.js
@@ -1,24 +1,27 @@
-// const mongoose = require('mongoose');
-// const db = require('../db');
-//
-// const Review = db.model('Review', new mongoose.Schema({
-//   restaurant_id: mongoose.Schema.ObjectId,
-//   date: String,
-//   review: String,
-//   users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-// }));
-//
-// exports.saveReview = (restaurantId, dateVisited, restaurantReview) => {
-//   const review = new Review({
-//     restaurant_id: restaurantId,
-//     date: dateVisited,
-//     review: restaurantReview,
-//   });
-//
-//   review.save((err, restaurant) => {
-//     if (err) {
-//       return console.error(err);
-//     }
-//     console.log(restaurant, ' is saved!');
-//   });
-// };
+const mongoose = require('mongoose');
+
+const userReview = new mongoose.Schema({
+  restaurant_id: mongoose.Schema.ObjectId,
+  date: String,
+  review: String,
+  users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+});
+
+userReview.statics.saveReview = (restaurantId, dateVisited, restaurantReview) => {
+  const review = new Review({
+    restaurant_id: restaurantId,
+    date: dateVisited,
+    review: restaurantReview,
+  });
+
+  review.save((err, restaurant) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log(restaurant, ' is saved!');
+  });
+};
+
+const Review = mongoose.model('Review', userReview);
+
+module.exports = Review;
