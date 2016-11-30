feb912d2458162697dd4ed63af8666e4bf2cdf70
Troy Maeder <troy.maeder@gmail.com>
2016-11-30 17:37:09 +0000

    Add function to highlight circles on review page

--- public/js/rate.js
+++ public/js/rate.js
@@ -1,79 +1,99 @@
-var count = 0;
-var count1 = 0;
-var count2 = 0;
-var count3 = 0;
-var count4 = 0;
-var ratingCounter = 0;
-
-function evenNumber(elementIndex) {
-	document.getElementsByClassName('circle')[elementIndex].style.backgroundColor = "white";
-	ratingCounter = ratingCounter - 1;
-	document.getElementsByName('ratings_five')[0].value = ratingCounter;
-
-}
-
-function oddNumber(elementIndex) {
-	document.getElementsByClassName('circle')[elementIndex].style.backgroundColor = "#589442";
-	ratingCounter = ratingCounter + 1;
-	document.getElementsByName('ratings_five')[0].value = ratingCounter;
-}
-
-document.getElementsByClassName('circle')[0].addEventListener('click', function() {
-	if (count % 2 === 0) {
-        oddNumber(0);
-    }
-    else {
-        evenNumber(0);
-    }
-
-    count++;
-});
-
-document.getElementsByClassName('circle')[1].addEventListener('click', function() {
-	if (count1 % 2 === 0) {
-        oddNumber(1);
-    }
-    else {
-        evenNumber(1);
-    }
-
-    count1++;
-});
-
-document.getElementsByClassName('circle')[2].addEventListener('click', function() {
-	if (count2 % 2 === 0) {
-        oddNumber(2);
-    }
-    else {
-        evenNumber(2);
-    }
-
-    count2++;
-});
-
-document.getElementsByClassName('circle')[3].addEventListener('click', function() {
-	if (count3 % 2 === 0) {
-        oddNumber(3);
-    }
-    else {
-        evenNumber(3);
-    }
-
-    count3++;
-});
-
-document.getElementsByClassName('circle')[4].addEventListener('click', function() {
-	if (count4 % 2 === 0) {
-        oddNumber(4);
-    }
-    else {
-        evenNumber(4);
-    }
-
-    count4++;
-});
+let ifClicked0 = false;
+let ifClicked1 = false;
+let ifClicked2 = false;
+let ifClicked3 = false;
+let ifClicked4 = false;
 
 $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
   });
+
+document.getElementsByClassName('circle')[0].addEventListener('click', function() {
+
+	if (ifClicked0 == true) {
+		for (var i = 1; i < 5; i++) {
+			console.log(' it is true');
+			document.getElementsByClassName('circle')[i].style.backgroundColor = 'white';
+			// document.getElementsByClassName('circle')[0].style.backgroundColor = "#589442";
+		}
+		ifClicked0 = false;
+		ifClicked1 = false;
+		ifClicked2 = false;
+		ifClicked3 = false;
+		ifClicked4 = false;
+	} else {
+		document.getElementsByClassName('circle')[0].style.backgroundColor = "#589442";
+		ifClicked0 = true;
+  }
+});
+
+
+
+
+document.getElementsByClassName('circle')[1].addEventListener('click', function() {
+	if (ifClicked1 == true) {
+		for (var i = 2; i < 5; i++) {
+			document.getElementsByClassName('circle')[i].style.backgroundColor = 'white';
+		}
+		ifClicked2 = false;
+		ifClicked3 = false;
+		ifClicked4 = false;
+	} else {
+  	for (var i = 1; i >= 0; i--) {
+			document.getElementsByClassName('circle')[i].style.backgroundColor = "#589442";
+		}
+		ifClicked0 = true;
+		ifClicked1 = true;
+  }
+});
+
+document.getElementsByClassName('circle')[2].addEventListener('click', function() {
+	if (ifClicked2 == true) {
+		for (var i = 3; i < 5; i++) {
+			document.getElementsByClassName('circle')[i].style.backgroundColor = "white";
+  	}
+		ifClicked3 = false;
+		ifClicked4 = false;
+	} else {
+		for (var i = 2; i >= 0; i--) {
+			document.getElementsByClassName('circle')[i].style.backgroundColor = "#589442";
+		}
+		ifClicked0 = true;
+		ifClicked1 = true;
+		ifClicked2 = true;
+  }
+});
+
+document.getElementsByClassName('circle')[3].addEventListener('click', function() {
+	if (ifClicked3 == true) {
+		for (var i = 4; i < 5; i++) {
+			document.getElementsByClassName('circle')[i].style.backgroundColor = "white";
+  	}
+		ifClicked4 = false;
+
+	} else {
+		for (var i = 3; i >= 0; i--) {
+			document.getElementsByClassName('circle')[i].style.backgroundColor = "#589442";
+		}
+		ifClicked0 = true;
+		ifClicked1 = true;
+		ifClicked2 = true;
+		ifClicked3 = true;
+  }
+});
+
+document.getElementsByClassName('circle')[4].addEventListener('click', function() {
+	if (ifClicked4 == true) {
+			document.getElementsByClassName('circle')[i].style.backgroundColor = "white";
+			ifClicked4 = false;
+	} else {
+		for (var i = 4; i >= 0; i--) {
+			document.getElementsByClassName('circle')[i].style.backgroundColor = "#589442";
+		}
+		ifClicked0 = true;
+		ifClicked1 = true;
+		ifClicked2 = true;
+		ifClicked3 = true;
+	}
+});
