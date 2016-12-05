e723ca5e833c8024567ad8ca78b31b57c01cb57e
Troy Maeder <troy.maeder@gmail.com>
2016-12-05 15:07:45 +0000

    Add different routes for first time users, clean up css on groups page

--- server.js
+++ server.js
@@ -69,9 +69,10 @@ app.get('/auth/facebook/callback', passport.authenticate('facebook', {
         console.log(err);
         // res.redirect('/error');
       });
+      res.redirect('/restaurant_page');
+    } else {
+      res.redirect('/default_restaurants');
     }
-
-    res.redirect('/restaurant_page');
   });
 
 app.get('/', (req, res) => {
@@ -118,6 +119,25 @@ app.get('/restaurant_page', (req, res) => {
     });
   });
 });
+
+app.get('/default_restaurants', (req, res) => {
+  res.render('default_restaurants', {
+    tgi_pic: defaultRestaurants[0].picture,
+    tgi: defaultRestaurants[0].name,
+    tgi_address: defaultRestaurants[0].address,
+    tgi_neighbourhood: defaultRestaurants[0].neighbourhood,
+    gauchos_pic: defaultRestaurants[1].picture,
+    gauchos: defaultRestaurants[1].name,
+    gauchos_address: defaultRestaurants[1].address,
+    gauchos_neighbourhood: defaultRestaurants[1].neighbourhood,
+    sushi_samba_pic: defaultRestaurants[2].picture,
+    sushi_samba: defaultRestaurants[2].name,
+    sushi_samba_address: defaultRestaurants[2].address,
+    sushi_samba_neighbourhood: defaultRestaurants[2].neighbourhood,
+    picture: 'https://www.google.com/maps/embed/v1/place?key=AIzaSyAVc4xMroGAOiRjn5-5rJmCdqvzxo73VIU&q=Space+Needle,Seattle+WA',
+  });
+});
+
 
 app.get('/search', (req, res) => {
   res.render('search');
